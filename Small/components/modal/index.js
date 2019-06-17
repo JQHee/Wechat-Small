0991aa71a5360f17d2dc37c7265d1c408718e461
HJQ <v-huangjiaqian@midland.local>
2019-06-17 16:55:40 +0800

    增加modal弹窗

---
+++ Small/components/modal/index.js
@@ -0,0 +1,66 @@
+/**
+ * 自定义modal浮层
+ * 使用方法：
+ * <modal show="{{showModal}}" height='60%' bindcancel="modalCancel" bindconfirm='modalConfirm'>
+     <view>你自己需要展示的内容</view>
+  </modal>
+
+  // 如果不想要下面的确定取消两个按钮，内容区的所有内容都要外部传入，可以这样写
+  <view class='mask' wx:if='{{show}}' bindtap='clickMask'>
+   <slot></slot>
+  </view>
+
+  属性说明：
+  show： 控制modal显示与隐藏
+  height：modal的高度
+  bindcancel：点击取消按钮的回调函数
+  bindconfirm：点击确定按钮的回调函数
+
+  使用模块：
+  场馆 -> 发布 -> 选择使用物品
+ */
+
+Component({
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    //是否显示modal
+    show: {
+      type: Boolean,
+      value: false
+    },
+    //modal的高度
+    height: {
+      type: String,
+      value: '80%'
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    clickMask() {
+      // this.setData({show: false})
+    },
+
+    cancel() {
+      this.setData({ show: false })
+      this.triggerEvent('cancel')
+    },
+
+    confirm() {
+      this.setData({ show: false })
+      this.triggerEvent('confirm')
+    }
+  }
+})
---
+++ readme.md
@@ -0,0 +1,7 @@
+#### 视频教程
+- tp5小程序后台 同一个人有上、中、下视频教程： https://www.bilibili.com/video/av37330135/?p=1&share_source=qq&share_medium=iphone&bbid=946d569a44c01d741aae8df6da3e6533&ts=1560648042
+- nodej后台视频教程 https://www.bilibili.com/video/av52704050/?p=1&share_source=qq&share_medium=iphone&bbid=946d569a44c01d741aae8df6da3e6533&ts=1560648595
+- 商业实战：https://www.bilibili.com/video/av49381039/?p=1&share_source=qq&share_medium=iphone&bbid=946d569a44c01d741aae8df6da3e6533&ts=1560653030
+
+#### wepy
+- https://github.com/Tencent/wepy
